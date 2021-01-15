08077876148c5bf84cfb467d3a82526ad615cd1b
Mireya <gmczgz20@gmail.com>
2021-01-15 16:50:49 +0100

    Calculadora de suma

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+function suma(){
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+    let resultado = numero1+numero2;
+
+    document.getElementById("resultado").value = resultado;
+}
